4f4eaac55129289d30fc83b6b5f52a51e25ba387
Francesco <francesco.falchi@gmail.com>
2017-10-24 11:53:57 +0200

    Add java file

---
+++ javaexercises_1.js
@@ -0,0 +1,29 @@
+
+var newArray = [77, 82, 9]
+newArray[1]
+// result: 82
+
+typeof(123/12) == "number"
+// result: true
+
+if (string == "Hello"){
+  true;
+}
+else{
+  false;
+}
+// true
+// Hello
+
+myArray = ['John', 'Bruce', 'William']
+// (3) ["John", "Bruce", "William"]
+emptyArray = []
+// []
+myArray.length;
+// 3
+for (var n=0, l = myArray.length; n<l; n++) {
+        emptyArray.push(myArray[n]);
+}
+// 3
+// calling emptyArray ==> ["John", "Bruce", "William"]
+//
